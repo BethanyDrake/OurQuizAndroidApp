73a61e94293337733a33fd2004512a783809d9d4
Bethany Drake <bethanyd@thoughtworks.com>
2020-05-22 15:56:33 +1000

    when I enter a quiz id an click join, it says no or okay.

--- app/src/main/java/com/sycorax/ourquiz/MainActivity.kt
+++ app/src/main/java/com/sycorax/ourquiz/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sycorax.ourquiz
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import com.android.volley.Request
 import com.android.volley.Response
@@ -16,16 +17,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun connect(view: View) {
+    fun join(view: View) {
         val textView: TextView = findViewById(R.id.textView)
         textView.setText("Connecting...")
+
+
+        val quizIdInputView: EditText = findViewById(R.id.editText)
+
         val queue = Volley.newRequestQueue(this)
-        val url = "http://10.0.2.2:8090/hello"
+        val url = "http://10.0.2.2:8090/join?quizId="+ quizIdInputView.text
 
         val stringRequest = StringRequest(
             Request.Method.GET, url,
             Response.Listener<String> { response ->
-                textView.text = "Response is: ${response}"
+                textView.text = response
             },
             Response.ErrorListener { textView.text = "That didn't work!" })
 
