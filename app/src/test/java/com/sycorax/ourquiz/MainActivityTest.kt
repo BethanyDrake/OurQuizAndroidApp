9b62e0fa25696800bad619fa971fb57d57448090
Bethany Drake <bethanyd@thoughtworks.com>
2020-06-05 12:36:12 +1000

    use mockk in MainActivityTets

--- app/src/test/java/com/sycorax/ourquiz/MainActivityTest.kt
+++ app/src/test/java/com/sycorax/ourquiz/MainActivityTest.kt
@@ -3,60 +3,43 @@ package com.sycorax.ourquiz
 import android.content.Context
 import android.content.Intent
 import android.view.View
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.spyk
+import io.mockk.verify
 import org.junit.Assert
 import org.junit.Test
 
 class MainActivityTest {
 
-    class MIntentFactory : IntentFactory() {
-        override fun <T> create(context: Context, activityClass: Class<T>): Intent {
-            return MockIntent(activityClass.simpleName)
-        }
-    }
-    class SpyMainActivity : MainActivity(MIntentFactory()) {
-        var startedActivity = false
-        var startedWith:Intent? = null
-        override fun startActivity(intent: Intent){
-            startedActivity = true
-            startedWith = intent
-        }
-    }
-
-    class MockIntent(private val tag:String) : Intent() {
-        override fun toString(): String {
-            return "Intent (mock) $tag"
-        }
-
-        override fun equals(other: Any?): Boolean {
-            return other.toString() == toString()
-        }
-
-        override fun hashCode(): Int {
-            return tag.hashCode()
-        }
-    }
-
     @Test
     fun join_opensJoinActivity() {
-        val mainActivity = SpyMainActivity()
+        val mIntent = mockk<Intent>()
+        val mIntentFactory = mockk<IntentFactory>()
+        every { mIntentFactory.create(any(), JoinActivity::class.java) } returns mIntent
+        val mainActivity = spyk(MainActivity(mIntentFactory))
+        every {mainActivity.startActivity(any())} returns Unit
 
         mainActivity.join(View(mainActivity))
 
-        val expectedIntent = MIntentFactory().create(mainActivity, JoinActivity::class.java)
-        val startedWithIntent = mainActivity.startedWith
-        Assert.assertTrue(mainActivity.startedActivity)
-        Assert.assertEquals(expectedIntent, startedWithIntent)
+        verify {
+            mainActivity.startActivity(mIntent)
+        }
     }
 
     @Test
     fun host_opensHostActivity() {
-        val mainActivity = SpyMainActivity()
+        val mIntent = mockk<Intent>()
+        val mIntentFactory = mockk<IntentFactory>()
+        every { mIntentFactory.create(any(), HostActivity::class.java) } returns mIntent
+        val mainActivity = spyk(MainActivity(mIntentFactory))
+        every {mainActivity.startActivity(any())} returns Unit
 
         mainActivity.host(View(mainActivity))
 
-        val expectedIntent = MIntentFactory().create(mainActivity, HostActivity::class.java)
-        val startedWithIntent = mainActivity.startedWith
-        Assert.assertTrue(mainActivity.startedActivity)
-        Assert.assertEquals(expectedIntent, startedWithIntent)
+        verify {
+            mainActivity.startActivity(mIntent)
+        }
+
     }
 }
